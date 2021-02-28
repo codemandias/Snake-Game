be21dd55a8a4695d5c90765bc87664ee416a88f3
Aks Chunara <ak278582@dal.ca>
2021-02-28 19:03:19 -0400

    Added gameover and intro screen, and wrote score update code

--- includes/snake-game.php
+++ includes/snake-game.php
@@ -1,26 +1,52 @@
-<body">
+<?php 
+    echo "<body ";
+    if(isset($_GET['gamestart'])){
+        echo "onload='init()'>";
+    } else {
+        echo ">";
+    }
+?>
     <h3>Snake Game</h3>
-        <div id="boundary">
-        <h3 id="score"></h3>
-        <h3 id="over"></h3>
+    <div id="boundary">
+        <h3 id="score">0</h3>
         <div class='board'>
-            <?php
-            // Feature: Implement Game Board/Boundary
-            // Implemented By: Aditya Sharma (B00827775)
-            // Learned: How to effectively output a matrix of rows and columnns by sending it through sever side
-            //			using php with valid id names/numbers. 
+        <!-- 
+            Feature: Displays the gameover and intro screen and synchs score with database 
+            Implementor: Aks Chunara
+        -->
+        <?php
+            //Checks if game is not over and has started
+            if(!isset($_GET['gameover']) && isset($_GET['gamestart'])){
+                require "./includes/game_boundry.php";
+            } else {
+                echo "<div id='screen' class='columns'>
+                            <div>";
+                            if(isset($_GET['gameover'])){
+                                //Displays gameover screen
+                                echo "<h3>Game Over!<br/>
+                                Better Luck Next Time<br/><br/></h3>
+                                <a href='./index.php?gamestart=1'><h4>Try Again</h4></a>";
 
-            echo "<table style='border-collapse: collapse;'>";
-            for($i=0;$i<30;$i++){
-                echo "<tr id='$i' class='rows'>";
-                for ($j = 0; $j < 30; $j++) {
-                    echo "<td id='$i - $j' class='columns'></td>";
-                }
-                echo "</tr>";
+                                //Synchs highscore with database
+                                $curr_score = $_GET['finalScore'];
+                                $querySQL = "SELECT `high_score` FROM `user-details`
+                                            WHERE `id` = " . $_SESSION['userID'];
+                                $result = $dbconnection->query($querySQL);
+                                if ($result->num_rows > 0 && $result->fetch_assoc()['high_score'] < $curr_score){ 
+                                    $querySQL = "UPDATE `user-details` SET `high_score` = ('$curr_score') 
+                                                WHERE `id` = " . $_SESSION['userID'].";";
+                                    $result = $dbconnection->query($querySQL);
+                                }
+                            } else {
+                                //Displays intro screen
+                                echo "<h3>Welcome To<br/>
+                                The Snake Game<br/><br/></h3>
+                                <a href='./index.php?gamestart=1'><h4>Start Game</h4></a>";
+                            }
+                echo        "</div>
+                    </div>";
             }
-            echo "</table>";
-            ?>
-            <button onclick="init()">Start Game</button>
+        ?>
         </div>
     </div>
 </body>
